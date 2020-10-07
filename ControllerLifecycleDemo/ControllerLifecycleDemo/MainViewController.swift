40628a6f329a834314d96cad452e6c3ae1189dac
Grzegorz Baczek <gb@cardlay.com>
2020-10-07 18:59:33 +0200

    cleanup

--- ControllerLifecycleDemo/ControllerLifecycleDemo/MainViewController.swift
+++ ControllerLifecycleDemo/ControllerLifecycleDemo/MainViewController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class MainViewController: DemoAppViewController {
+   /* @IBAction func btnClicked(_ sender: UIButton) {
+        if sender.title(for: .normal) == "X" {
+          sender.setTitle("A very long title for this button", for: .normal)
+        } else {
+          sender.setTitle("X", for: .normal)
+        }
+    }*/
+    @IBAction func btnClicked(_ sender: UIButton) {
+        if sender.title(for: .normal) == "X" {
+          sender.setTitle("A very long title for this button", for: .normal)
+        } else {
+          sender.setTitle("X", for: .normal)
+        }
+    }
+    
+    
     override var className : String {
         get{
             return "MainViewController"
