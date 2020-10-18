be7a5a54fa0b83cde491e2fdc6fcfcfcd9ac30e6
Emil Nielsen <emilqz@gmail.com>
2020-10-18 16:54:17 +0200

    Product Browser

---
+++ Lecture_7/Product Browser/Product Browser/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Product Browser
+//
+//  Created by Emil Nielsen on 18/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
