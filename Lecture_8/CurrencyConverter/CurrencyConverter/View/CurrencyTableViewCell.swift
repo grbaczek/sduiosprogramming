97bba2a59b6b4fa77925c67b703dca282936bf15
Emil Nielsen <emilqz@gmail.com>
2020-10-27 19:24:06 +0100

    Currency Converter

---
+++ Lecture_8/CurrencyConverter/CurrencyConverter/View/CurrencyTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CurrencyTableViewCell.swift
+//  CurrencyConverter
+//
+//  Created by Emil Nielsen on 27/10/2020.
+//
+
+import Foundation
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var conversion: UILabel!
+
+}
