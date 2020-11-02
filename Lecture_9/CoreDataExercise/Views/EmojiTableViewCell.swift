b12c3d2764ae4541a165a50d943bf2896dab843d
Emil Nielsen <emilqz@gmail.com>
2020-11-02 17:50:54 +0100

    CoreData exercise

---
+++ Lecture_9/CoreDataExercise/Views/EmojiTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  EmojiTableViewCell.swift
+//  CoreDataExercise
+//
+//  Created by Emil Nielsen on 31/10/2020.
+//  Copyright © 2020 Emil Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+        
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+ 
+}
