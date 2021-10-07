57346ebdaf70f06efff2c2d3ee1ca2eb50743089
Benjamin Staugaard <bemmamin@live.dk>
2021-10-07 15:15:28 +0200

    TicTacToeMultiView

---
+++ 2021/Lecture_5/TicTacToeMultiView/TicTacToeMultiView/ViewModels/BoardViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  BoardViewModel.swift
+//  TicTacToeMultiView
+//
+//  Created by Benjamin Staugaard on 07/10/2021.
+//
+
+import SwiftUI
+
+class BoardViewModel: ObservableObject {
+    
+    var player = 0
+    @Published var boardModel = BoardModel()
+    @Published var gameOver = false
+    @Published var gameOverText: String?
+    
+    func setField(index: Int) {
+        if (boardModel[index] == .empty) {
+            boardModel[index] = player == 0 ? .cross : .nought
+            if (boardModel.hasWinner()) {
+                gameOver = true
+                gameOverText = "Player \(player == 0 ? "X" : "O") won!"
+            } else if (boardModel.full()) {
+                gameOver = true
+                gameOverText = "Draw"
+            }
+            player = player == 0 ? 1 : 0
+        }
+    }
+    
+    func reset() {
+        player = 0
+        boardModel.reset()
+    }
+}
---
+++ 2021/Lecture_5/TicTacToeMultiView/TicTacToeMultiView/Views/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  TicTacToeMultiView
+//
+//  Created by Benjamin Staugaard on 07/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var boardViewModel = BoardViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Tic Tac Toe")
+                .font(.largeTitle)
+                .bold()
+            BoardView(boardViewModel: boardViewModel)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
